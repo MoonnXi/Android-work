1fdad9a306812c286c812327e3a3df9fbc881337
KOIANYU <3243851577@qq.com>
2024-10-08 23:19:42 +0800

    删除1

--- ImageSharing/app/src/main/java/com/imagesharing/util/HeadersUtil.java
+++
@@ -1,6 +0,0 @@
-package com.imagesharing.util;
-
-public class HeadersUtil {
-    public final static String APP_ID = "0c43d325bd4c4077a2ef71afd51a2ac9";
-    public final static String APP_SECRET = "1796311d4cb76a76e4c309e0ca1c6ff5a13c0";
-}
