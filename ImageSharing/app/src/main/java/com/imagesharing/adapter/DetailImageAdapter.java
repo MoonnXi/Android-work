c4abab991d8fea60670f43157f13869be7120dc3
KOIANYU <3243851577@qq.com>
2024-10-12 23:48:30 +0800

    一级评论版本2，整合到main

--- ImageSharing/app/src/main/java/com/imagesharing/adapter/DetailImageAdapter.java
+++ ImageSharing/app/src/main/java/com/imagesharing/adapter/DetailImageAdapter.java
@@ -1,66 +1,65 @@
 package com.imagesharing.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.imagesharing.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
-public class DetailImageAdapter extends BaseAdapter {
+public class DetailImageAdapter extends RecyclerView.Adapter<DetailImageAdapter.ImageViewHolder> {
 
-    private Context context;
-    private JSONArray imageUrlList;
+    private final Context context;
+    private final JSONArray imageUrlList;
 
     public DetailImageAdapter(Context context, JSONArray imageUrlList) {
         this.context = context;
         this.imageUrlList = imageUrlList;
     }
 
+    @NonNull
     @Override
-    public int getCount() {
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_detail_image, parent, false);
+        return new ImageViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        try {
+            String imageUrl = imageUrlList.getString(position);
+            Glide.with(context)
+                    .load(imageUrl)
+                    .into(holder.imageView);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
         return imageUrlList.length();
     }
 
-    @Override
-    public Object getItem(int i) {
-        try {
-            return imageUrlList.getString(i);
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    public static class ImageViewHolder extends RecyclerView.ViewHolder {
 
-    @Override
-    public long getItemId(int i) {
-        return i;
-    }
-
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
         ImageView imageView;
-        if (view == null) {
-            imageView = new ImageView(context);
+
+        public ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.iv_image);
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setLayoutParams(new GridView.LayoutParams(300, 300));
-        } else {
-            imageView = (ImageView) view;
+            //imageView.setLayoutParams(new GridView.LayoutParams(50, 50));
         }
-
-        // 使用 Glide 加载图片
-        try {
-            Glide.with(context)
-                    .load(imageUrlList.get(i))
-                    .into(imageView);
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
-
-        return imageView;
     }
 }
